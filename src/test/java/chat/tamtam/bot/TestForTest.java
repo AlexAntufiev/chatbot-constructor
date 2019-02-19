ba3016429fa46f3c4d2744e862ba5c138f8a2d78
Mikhail Nechaev <mikhail.nechaev@corp.mail.ru>
2019-02-20 01:04:34 +0300

    test for test

---
+++ src/test/java/chat/tamtam/bot/TestForTest.java
@@ -0,0 +1,18 @@
+package chat.tamtam.bot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author mikhail.nechaev
+ * Since 20/02/2019
+ */
+
+class TestForTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
