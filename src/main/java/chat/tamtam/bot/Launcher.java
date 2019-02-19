4dbde56af3fbbf28f144b209f472f7dde9475dc6
Mikhail Nechaev <mikhail.nechaev@corp.mail.ru>
2019-02-20 00:58:39 +0300

    init

---
+++ src/main/java/chat/tamtam/bot/Launcher.java
@@ -0,0 +1,16 @@
+package chat.tamtam.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mikhail.nechaev
+ * Since 19/02/2019
+ */
+@SpringBootApplication
+public class Launcher {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+}
