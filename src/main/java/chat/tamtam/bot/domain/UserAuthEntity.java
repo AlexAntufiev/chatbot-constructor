b549e1d9301d4a2e77627d54f2ef4d6e876ed0b4
Aleksey.Antufev <Aleksey.Antufev@billing.ru>
2019-03-03 20:13:10 +0300

    Insert admin/admin user to BD

--- src/main/java/chat/tamtam/bot/domain/UserAuthEntity.java
+++ src/main/java/chat/tamtam/bot/domain/UserAuthEntity.java
@@ -3,9 +3,11 @@ package chat.tamtam.bot.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class UserAuthEntity {
     private @NonNull String login;
     private @NonNull String password;
