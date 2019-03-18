69931daca80e8391dce7c3ddfd239871fad592b9
Alex Antufev <12766383+AlexAntufiev@users.noreply.github.com>
2019-03-18 23:06:53 +0300

    CC-64 (#72)
    * [Travis] append travis branch for build
    
    * [CC-64] Travis-docker deployment
    
    * try to run deploy in PR

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-app_name=chatbot-constructor
-path_to_jar="../build/libs/"
-jar_name="${app_name}.jar"
-jar=${path_to_jar}
-jar+=${jar_name}
-run_script="../scripts/run.sh"
-
 pem_key_file="../keys/CentOS_Basic-1-1-10_10GB_1_id_rsa.pem"
 user="centos"
 server="89.208.84.173"
 
-./build.sh
-
-echo "Jar: "+${jar}
-echo "Sending to: "+${server}
-rsync -az --info=progress2 -e "ssh -i ${pem_key_file}" ${jar} ${user}@${server}:/home/centos/chatbot-constructor/ && echo "*** Complete ***"
-rsync -az --info=progress2 -e "ssh -i ${pem_key_file}" ${run_script} ${user}@${server}:/home/centos/chatbot-constructor/ && echo "*** Complete ***"
-ssh -i ${pem_key_file} ${user}@${server} "cd ./${app_name} && ./run.sh -p development start"
+rsync -az --info=progress2 $1 ${user}@${server}:/home/centos/chatbot-constructor/
+#rsync -az --info=progress2 -e "ssh -i ${pem_key_file}" ${run_script} ${user}@${server}:/home/centos/chatbot-constructor/ && echo "*** Complete ***"
+#ssh -i ${pem_key_file} ${user}@${server} "cd ./${app_name} && ./run.sh -p development start"
