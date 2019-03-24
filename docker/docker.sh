8833aa8ab8613a149b35be10c4c9c0d538d2d390
Aleksey.Antufev <Aleksey.Antufev@billing.ru>
2019-03-24 22:02:16 +0300

    add notifications

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,11 +1,40 @@
 #!/usr/bin/env bash
 
 application_name="chatbot-constructor"
+token="QeFH2Jalc16THlx9YcNBDhtdx_u3uHOeDp8y8P20pT4"
+chat_id="-70460825709429"
+url="https://botapi.tamtam.chat/messages?access_token=${token}&chat_id=${chat_id}"
+server_name="TEST-1"
+body=""
 
-docker stop ${application_name}
-docker rm -f ${application_name}
-docker image rm -f ${application_name}
+function set_message(){
+    local message=$*
+    if [[ ${message} != "" ]];
+    then
+        body="{\"text\":\"${message}\"}"
+    else
+        echo "No message"
+    fi
+}
 
-docker build -t ${application_name} .
-docker run -d -p 8090:8090 --name ${application_name} ${application_name}
-echo "Application started"
+function send_message() {
+    set_message $*
+    echo ${body} | curl -d @- -H "Content-Type: application/json" -X POST ${url}
+}
+
+function main() {
+    send_message "Всем привет"
+    send_message "${server_name} останавливается"
+    docker stop ${application_name}
+    docker rm -f ${application_name}
+    docker image rm -f ${application_name}
+    send_message "${server_name} остановлен"
+
+    docker build -t ${application_name} .
+
+    send_message "${server_name} запускается"
+    docker run -d -p 8090:8090 --name ${application_name} ${application_name}
+    send_message "${server_name} запущен"
+}
+
+main
