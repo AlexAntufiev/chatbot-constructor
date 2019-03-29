9f85656cac36d2b73fb4a24fff6536d6b9731843
Alexey Kotelevskiy <alexey.kotelevskiy.ext@nokia.com>
2019-03-29 18:59:59 +0300

    default bot icon

--- src/main/js/app/components/chatChannelCard.jsx
+++ src/main/js/app/components/chatChannelCard.jsx
@@ -1,6 +1,7 @@
 import {Card} from "primereact/card";
 import React from "react";
 import {OverlayPanel} from "primereact/overlaypanel";
+import customBotIcon from "images/customBotAvatar.jpg";
 
 export default class ChatChannelCard extends React.Component {
 
@@ -10,10 +11,12 @@ export default class ChatChannelCard extends React.Component {
             const tokens = this.props.chatChannel.link.split('/');
             channelLinkName = '@' + tokens[tokens.length - 1];
         }
+        if (!this.props.chatChannel.icon) {
+            this.props.chatChannel.icon = customBotIcon;
+        }
         const header = (
             <div>
-                {this.props.chatChannel.icon &&
-                <img src={this.props.chatChannel.icon} className="channel-list-element_image"/>}
+                <img src={this.props.chatChannel.icon} className="channel-list-element_image"/>
                 {this.props.chatChannel.link &&
                 <a href={this.props.chatChannel.link} target="_blank"
                    className="channel-list-element_link">{channelLinkName}</a>}
@@ -29,8 +32,7 @@ export default class ChatChannelCard extends React.Component {
                 <OverlayPanel ref={(el) => this.overlayPanel = el} appendTo={document.body}
                               className={"channel-list-element_overlay-panel"}>
                     <div className="image-container">
-                        {this.props.chatChannel.icon &&
-                        <img src={this.props.chatChannel.icon} className="channel-list-element_overlay-panel_image"/>}
+                        <img src={this.props.chatChannel.icon} className="channel-list-element_overlay-panel_image"/>
                     </div>
                     <div>
                         <div className="channel-list-element_overlay-panel_title">{this.props.chatChannel.title}</div>
