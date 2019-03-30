bbe713be33b02bee7053e685d2aa1c17396a6145
Aleksey.Antufev <Aleksey.Antufev@billing.ru>
2019-03-30 16:15:24 +0300

    [CC-91] Set up test profile

--- src/main/java/chat/tamtam/bot/Launcher.java
+++ src/main/java/chat/tamtam/bot/Launcher.java
@@ -1,17 +1,11 @@
 package chat.tamtam.bot;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import chat.tamtam.bot.configuration.Profiles;
-import chat.tamtam.bot.domain.user.UserAuthEntity;
-import chat.tamtam.bot.service.UserService;
 import lombok.AllArgsConstructor;
 
 @SpringBootApplication
@@ -20,17 +14,9 @@ import lombok.AllArgsConstructor;
 @EnableAsync
 public class Launcher {
 
-    private final UserService userService;
-
     public static void main(final String[] args) {
         SpringApplication springApplication = new SpringApplication(Launcher.class);
         springApplication.addListeners(new ApplicationPidFileWriter("chatbot-constructor.pid"));
         springApplication.run(args);
-    }
-
-    @Bean
-    @Profile(Profiles.DEVELOPMENT)
-    InitializingBean populateDatabase() {
-        return () -> userService.addUser(new UserAuthEntity("admin", "admin"));
     }
 }
--- src/main/java/chat/tamtam/bot/service/UserService.java
+++ src/main/java/chat/tamtam/bot/service/UserService.java
@@ -1,8 +1,12 @@
 package chat.tamtam.bot.service;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import chat.tamtam.bot.configuration.Profiles;
 import chat.tamtam.bot.configuration.logging.Loggable;
 import chat.tamtam.bot.domain.user.UserAuthEntity;
 import chat.tamtam.bot.domain.user.UserEntity;
@@ -36,5 +40,22 @@ public class UserService {
     @Loggable
     public long getUserIdByToken(final String token) {
         return sessionRepository.findByToken(token).getUserId();
+    }
+
+    @Bean
+    @Profile(Profiles.DEVELOPMENT)
+    InitializingBean populateDevelopmentDatabase() {
+        return () -> addUser(new UserAuthEntity("admin", "admin"));
+    }
+
+    @Bean
+    @Profile(Profiles.TEST)
+    InitializingBean populateTestDatabase() {
+        return () -> {
+            addUser(new UserAuthEntity("test1", "B&]LhfC2bP^Joh.{b,g\\b\"a,%)'DW;x\""));
+            addUser(new UserAuthEntity("test2", "_vq#4g7K<w*k*hFRuX]upQ2Grz{_K?WH"));
+            addUser(new UserAuthEntity("test3", "fP6~)w=U]d=FL&5s=E{$w`=ej[]',,5,"));
+            addUser(new UserAuthEntity("test4", "}NeP#<A6J(a$LM/3Yh\\-/;oTf~`#Y?uJ"));
+        };
     }
 }
