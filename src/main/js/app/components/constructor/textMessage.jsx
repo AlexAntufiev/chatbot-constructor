b413778a3550f028a4a09bc4e5dcc949498a7faa
Alexey Kotelevskiy <alexey.kotelevskiy.ext@nokia.com>
2019-04-01 23:12:13 +0300

    [CC-113] Frontend should send only that fields of broadcast message which have to be updated

--- src/main/js/app/components/constructor/textMessage.jsx
+++ src/main/js/app/components/constructor/textMessage.jsx
@@ -5,67 +5,99 @@ import {Button} from "primereact/button";
 import React from "react";
 import * as BroadcastMessageService from "app/service/broadcastMessage";
 import {Growl} from "primereact/growl";
-import {connect} from "react-redux";
-import {injectIntl} from "react-intl";
 import * as AxiosMessages from 'app/utils/axiosMessages';
 import * as Routes from 'app/constants/routes';
 import makeUrl from "app/utils/makeUrl";
-import {withRouter} from "react-router";
 import BroadcastMessageState from 'app/utils/broadcastMessageState';
 import shallowequal from "shallowequal";
 import getCalendar from "app/i18n/calendarLocale";
+import {withRouter} from "react-router";
+import {connect} from "react-redux";
+import {injectIntl} from "react-intl";
 
 class TextMessage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: "",
-            firingTime: null,
-            erasingTime: null,
-            text: "",
+            message: {
+                title: "",
+                text: "",
+                firingTime: null,
+                erasingTime: null
+            },
+            initialMessage: {
+                title: "",
+                text: "",
+                firingTime: null,
+                erasingTime: null
+            },
             ajaxUpdateProcess: false,
             ajaxRemoveProcess: false
         };
         this.onUpdateMessage = this.onUpdateMessage.bind(this);
         this.onRemoveMessage = this.onRemoveMessage.bind(this);
+        this.getUpdatedFields = this.getUpdatedFields.bind(this);
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         const message = this.props.message;
         if (message && !shallowequal(prevProps.message, message)) {
-            this.setState({
+            const firingTime = new Date(message.firingTime);
+            const erasingTime = new Date(message.erasingTime);
+            const newMessage = {
                 title: message.title,
-                firingTime: message.firingTime ? new Date(message.firingTime) : null,
-                erasingTime: message.erasingTime ? new Date(message.erasingTime) : null,
+                firingTime: message.firingTime ? firingTime.getTime() : null,
+                erasingTime: message.erasingTime ? erasingTime.getTime() : null,
                 text: message.text ? message.text : "",
+            };
+            this.setState({
+                message: Object.assign({}, newMessage),
+                initialMessage: Object.assign({}, newMessage),
                 ajaxUpdateProcess: false,
                 ajaxRemoveProcess: false
             });
         }
     }
 
+    getUpdatedFields() {
+        let message = Object.assign({}, this.state.message);
+        for (let key in message) {
+            if (message[key] === this.state.initialMessage[key]) {
+                delete message[key];
+            }
+        }
+        return message;
+    }
+
     onUpdateMessage() {
-        if (this.state.title.trim() === '') {
+        if (this.state.message.title.trim() === '') {
             AxiosMessages.serverErrorResponse(this, 'errors.broadcast.message.title.is.empty');
             return;
         }
 
-        if (this.props.message.state === BroadcastMessageState.CREATED && this.state.text.trim() && !this.state.firingTime) {
+        if (this.props.message.state === BroadcastMessageState.CREATED && this.state.message.text.trim() && !this.state.message.firingTime) {
             AxiosMessages.serverErrorResponse(this, 'error.broadcast.message.need.fill.text.and.posttime');
             return;
         }
 
         this.setState({ajaxUpdateProcess: true});
-        BroadcastMessageService.updateBroadcastMessage(this.props.botSchemeId, this.props.chatChannelId, this.props.message.id, {
-                title: this.state.title,
-                text: this.state.text,
-                firingTime: this.state.firingTime ? this.state.firingTime.toUTCString() : null,
-                erasingTime: this.state.erasingTime ? this.state.erasingTime.toUTCString() : null,
-            },
+        let message = this.getUpdatedFields();
+        //time fields send always
+        if (this.state.message.firingTime) {
+            message.firingTime = new Date(this.state.message.firingTime).toUTCString();
+        }
+        if (this.state.message.erasingTime) {
+            message.erasingTime = new Date(this.state.message.erasingTime).toUTCString();
+        }
+        BroadcastMessageService.updateBroadcastMessage(this.props.botSchemeId, this.props.chatChannelId, this.props.message.id,
+            message,
             () => {
                 this.props.refreshMessageList();
                 AxiosMessages.successOperation(this, 'app.broadcastmessage.saved');
-                this.setState({ajaxUpdateProcess: false});
+                this.setState({
+                    ajaxUpdateProcess: false,
+                    initialMessage: this.state.message
+                });
             },
             () => this.setState({ajaxUpdateProcess: false}),
             this);
@@ -97,37 +129,51 @@ class TextMessage extends React.Component {
             return (<div/>);
         }
         const {intl} = this.props;
+        const changeMessageField = (field, value) => {
+            let message = this.state.message;
+            message[field] = value;
+            this.setState({message: message});
+        };
         return (
             <div className='text-card'>
                 <Growl ref={(el) => this.growl = el}/>
                 <div className="text-card_detail-element">
-                    <InputText placeholder={intl.formatMessage({id: 'app.dialog.name'})} value={this.state.title}
-                               onChange={(e) => this.setState({title: e.target.value})}/>
+                    <InputText placeholder={intl.formatMessage({id: 'app.dialog.name'})}
+                               value={this.state.message.title}
+                               onChange={(e) => changeMessageField('title', e.target.value)}/>
                 </div>
                 <div className="text-card_detail-element">
                     <Calendar showTime={true} showSeconds={false}
                               placeholder={intl.formatMessage({id: 'app.broadcastmessage.postingtime'})}
                               disabled={this.props.message.state === BroadcastMessageState.SENT}
-                              onChange={(e) => this.setState({firingTime: e.value})}
+                              onChange={(e) => {
+                                  const newDate = e.value;
+                                  newDate.setSeconds(0);
+                                  changeMessageField('firingTime', newDate.getTime());
+                              }}
                               dateFormat={"D, d M yy"}
                               hourFormat={this.props.locale === 'ru' ? "24" : "12"}
-                              value={this.state.firingTime}
+                              value={this.state.message.firingTime ? new Date(this.state.message.firingTime) : null}
                               locale={getCalendar(this.props.locale)}/>
                 </div>
                 <div className="text-card_detail-element">
                     <Calendar showTime={true} showSeconds={false}
                               placeholder={intl.formatMessage({id: 'app.broadcastmessage.erasingtime'})}
-                              onChange={(e) => this.setState({erasingTime: e.value})}
+                              onChange={(e) => {
+                                  const newDate = e.value;
+                                  newDate.setSeconds(0);
+                                  changeMessageField('erasingTime', newDate.getTime());
+                              }}
                               dateFormat={"D, d M yy"}
                               hourFormat={this.props.locale === 'ru' ? "24" : "12"}
-                              value={this.state.erasingTime}
+                              value={this.state.message.erasingTime ? new Date(this.state.message.erasingTime) : null}
                               locale={getCalendar(this.props.locale)}/>
                 </div>
                 <div className="text-card_detail-element">
                     <InputTextarea placeholder={intl.formatMessage({id: 'app.broadcastmessage.text'})}
-                                   value={this.state.text} rows={5} cols={60}
+                                   value={this.state.message.text} rows={5} cols={60}
                                    disabled={this.props.message.state === BroadcastMessageState.SENT}
-                                   onChange={(e) => this.setState({text: e.target.value})}
+                                   onChange={(e) => changeMessageField('text', e.target.value)}
                                    autoResize={true}
                                    maxLength={3000}/>
                 </div>
@@ -135,7 +181,7 @@ class TextMessage extends React.Component {
                     <Button
                         label={intl.formatMessage({id: 'app.dialog.save'})}
                         onClick={this.onUpdateMessage}
-                        disabled={this.state.ajaxUpdateProcess}/>
+                        disabled={this.state.ajaxUpdateProcess || Object.keys(this.getUpdatedFields()).length === 0}/>
                     <Button label={intl.formatMessage({id: 'app.bot.remove'})} onClick={this.onRemoveMessage}
                             disabled={this.state.ajaxRemoveProcess}/>
                 </div>
