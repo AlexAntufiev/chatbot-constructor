196098bff4cbbc5ae4fc52a3f7e0a965a129cd11
Alexey Kotelevskiy <alexey.kotelevskiy.ext@nokia.com>
2019-04-08 17:04:30 +0300

    [CC-125] Images attacment

--- src/main/js/app/components/constructor/textMessage.jsx
+++ src/main/js/app/components/constructor/textMessage.jsx
@@ -4,6 +4,7 @@ import {InputTextarea} from "primereact/inputtextarea";
 import {Button} from "primereact/button";
 import React from "react";
 import * as BroadcastMessageService from "app/service/broadcastMessage";
+import * as TamBotService from "app/service/tamBot";
 import {Growl} from "primereact/growl";
 import * as AxiosMessages from 'app/utils/axiosMessages';
 import * as Routes from 'app/constants/routes';
@@ -23,7 +24,7 @@ class TextMessage extends React.Component {
                 title: "",
                 text: "",
                 firingTime: null,
-                erasingTime: null
+                erasingTime: null,
             },
             initialMessage: {
                 title: "",
@@ -31,17 +32,40 @@ class TextMessage extends React.Component {
                 firingTime: null,
                 erasingTime: null
             },
+            uploadLink: "",
+            attachments: [],
+            attachmentsWasChanged: false,
+            ajaxUploadAttachProcess: false,
             ajaxUpdateProcess: false,
             ajaxRemoveProcess: false
         };
         this.onUpdateMessage = this.onUpdateMessage.bind(this);
         this.onRemoveMessage = this.onRemoveMessage.bind(this);
         this.getUpdatedFields = this.getUpdatedFields.bind(this);
+        this.uploadFilesToTam = this.uploadFilesToTam.bind(this);
+        this.createAttachElementsList = this.createAttachElementsList.bind(this);
+    }
+
+    componentDidMount() {
+        TamBotService.getAttachmentUploadLink(this.props.botSchemeId, "photo",
+            (res) => this.setState({uploadLink: res.data.payload.url}), null, this);
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         const message = this.props.message;
         if (message && !shallowequal(prevProps.message, message)) {
+            this.uploadButton.value = "";
+            BroadcastMessageService.getAttacmentsList(this.props.botSchemeId, this.props.chatChannelId, this.props.message.id, (res) => {
+                let attachments = [];
+                res.data.payload.forEach((attach) => {
+                    attachments.push({
+                        title: attach.title,
+                        id: attach.id,
+                        token: attach.attachmentIdentifier
+                    });
+                });
+                this.setState({attachments: attachments});
+            });
             const firingTime = new Date(message.firingTime);
             const erasingTime = new Date(message.erasingTime);
             const newMessage = {
@@ -54,7 +78,9 @@ class TextMessage extends React.Component {
                 message: Object.assign({}, newMessage),
                 initialMessage: Object.assign({}, newMessage),
                 ajaxUpdateProcess: false,
-                ajaxRemoveProcess: false
+                ajaxRemoveProcess: false,
+                attachmentsWasChanged: false,
+                ajaxUploadAttachProcess: false
             });
         }
     }
@@ -67,6 +93,62 @@ class TextMessage extends React.Component {
             }
         }
         return message;
+    }
+
+    createAttachElementsList() {
+        let renderedList = [];
+        for (let i = 0; i < this.state.attachments.length; i++) {
+            const attach = this.state.attachments[i];
+            if (!attach.removed) {
+                renderedList.push(<div className={"attach-element"}>
+                    <div className={"table-cell attach-title"}>{attach.title}</div>
+                    <div className={"table-cell"}><Button icon={"pi pi-times"} onClick={() => {
+                        const attachments = [...this.state.attachments];
+                        attachments[i].removed = true;
+                        this.setState({
+                            attachments: attachments,
+                            attachmentsWasChanged: true
+                        });
+                    }}/></div>
+                </div>)
+            }
+        }
+        return renderedList;
+    }
+
+    uploadFilesToTam(files) {
+        console.log(files);
+        this.setState({
+            ajaxUploadAttachProcess: true,
+            attachmentsWasChanged: true
+        });
+        let uploaded = 0;
+        for (let i = 0; i < files.length; ++i) {
+            const data = new FormData();
+            const xhr = new XMLHttpRequest();
+            data.append('data', files[i]);
+            xhr.open('POST', this.state.uploadLink, true);
+            const self = this;
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                    const uploadedObj = JSON.parse(xhr.responseText);
+                    const firstKey = Object.keys(uploadedObj["photos"])[0];
+                    self.setState({
+                        attachments: [...self.state.attachments, {
+                            uploadedFileId: i,
+                            title: files[i].name,
+                            img: URL.createObjectURL(files[i]),
+                            token: uploadedObj["photos"][firstKey].token
+                        }]
+                    });
+                }
+                uploaded++;
+                if (uploaded === files.length) {
+                    self.setState({ajaxUploadAttachProcess: false});
+                }
+            };
+            xhr.send(data);
+        }
     }
 
     onUpdateMessage() {
@@ -92,11 +174,25 @@ class TextMessage extends React.Component {
         BroadcastMessageService.updateBroadcastMessage(this.props.botSchemeId, this.props.chatChannelId, this.props.message.id,
             message,
             () => {
+                this.state.attachments.forEach((attach) => {
+                    if (!attach.id && !attach.removed) {
+                        BroadcastMessageService.addAttachment(this.props.botSchemeId, this.props.chatChannelId, this.props.message.id, {
+                            title: attach.title,
+                            token: attach.token,
+                            type: "photo"
+                        });
+                    } else {
+                        if (attach.removed) {
+                            BroadcastMessageService.removeAttachment(this.props.botSchemeId, this.props.chatChannelId, this.props.message.id, attach.id);
+                        }
+                    }
+                });
                 this.props.refreshMessageList();
                 AxiosMessages.successOperation(this, 'app.broadcastmessage.saved');
                 this.setState({
                     ajaxUpdateProcess: false,
-                    initialMessage: Object.assign({}, this.state.message)
+                    initialMessage: Object.assign({}, this.state.message),
+                    attachmentsWasChanged: false
                 });
             },
             () => this.setState({ajaxUpdateProcess: false}),
@@ -115,7 +211,6 @@ class TextMessage extends React.Component {
                         chatChannelId: this.props.chatChannelId
                     });
                     this.props.refreshMessageList();
-                    AxiosMessages.successOperation(this, 'success.tam.bot.name.changed');
                     this.setState({ajaxRemoveProcess: false});
                     this.props.history.push(url);
                 }, () => this.setState({ajaxRemoveProcess: false}),
@@ -134,6 +229,12 @@ class TextMessage extends React.Component {
             message[field] = value;
             this.setState({message: message});
         };
+
+        const attachments = this.createAttachElementsList();
+        let disableSave = this.state.ajaxUpdateProcess || Object.keys(this.getUpdatedFields()).length === 0;
+        if (this.state.attachmentsWasChanged) {
+            disableSave = false;
+        }
         return (
             <div className='text-card'>
                 <Growl ref={(el) => this.growl = el}/>
@@ -183,11 +284,20 @@ class TextMessage extends React.Component {
                                    autoResize={true}
                                    maxLength={3000}/>
                 </div>
+                <div className="text-card_detail-element">
+                    <Button icon={"pi pi-paperclip"} label={"Attach"} onClick={() => this.uploadButton.click()}
+                            disabled={this.state.ajaxUploadAttachProcess}/>
+                    <input accept="image/*" className={"attach-input"} name={"data"} ref={(obj) => this.uploadButton = obj} type={"file"}
+                           multiple={true} onInput={(event) => this.uploadFilesToTam(event.target.files)}/>
+                    <div className={"attach-container"}>
+                        {attachments}
+                    </div>
+                </div>
                 <div className="text-card_button-panel">
                     <Button
                         label={intl.formatMessage({id: 'app.dialog.save'})}
                         onClick={this.onUpdateMessage}
-                        disabled={this.state.ajaxUpdateProcess || Object.keys(this.getUpdatedFields()).length === 0}/>
+                        disabled={disableSave}/>
                     <Button label={intl.formatMessage({id: 'app.bot.remove'})} onClick={this.onRemoveMessage}
                             disabled={this.state.ajaxRemoveProcess}/>
                 </div>
--- src/main/js/app/service/tamBot.jsx
+++ src/main/js/app/service/tamBot.jsx
@@ -7,3 +7,8 @@ export function getTamBot(botId, callbackSuccess, callbackFail, context) {
     const url = makeUrl(ApiPoints.BOT_STATUS, {id: botId});
     handleRequest(axios.get(url), callbackSuccess, callbackFail, context);
 }
+
+export function getAttachmentUploadLink(botId, attachmentType, callbackSuccess, callbackFail, context) {
+    const url = makeUrl(ApiPoints.GET_UPLOAD_ATTACHMENT_LINK, {id:botId, attachmentType: attachmentType});
+    handleRequest(axios.get(url), callbackSuccess, callbackFail, context);
+}
