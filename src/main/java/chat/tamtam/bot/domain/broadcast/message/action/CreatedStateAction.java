17534de1bbd43e0eb911e6e964032e983b84da12
gorbatov <gorbatov.eg@gmail.com>
2019-04-22 21:25:13 +0300

    Timezone fix, java.sql.timestamp -> java.time.instant.

--- src/main/java/chat/tamtam/bot/domain/broadcast/message/action/CreatedStateAction.java
+++ src/main/java/chat/tamtam/bot/domain/broadcast/message/action/CreatedStateAction.java
@@ -1,5 +1,6 @@
 package chat.tamtam.bot.domain.broadcast.message.action;
 
+import java.time.Instant;
 import java.time.ZonedDateTime;
 
 import org.springframework.stereotype.Component;
@@ -42,36 +43,27 @@ public final class CreatedStateAction extends BroadcastMessageStateAction {
                 );
             }
 
-            ZonedDateTime firingTime = getDateTimeAtLocalZone(
-                    broadcastMessageUpdate.getFiringTime(),
-                    Error.BROADCAST_MESSAGE_FIRING_TIME_IS_MALFORMED
-            );
-
-            ZonedDateTime currentTime = ZonedDateTime.now(SERVER_LOCAL_ZONE_ID);
+            Instant currentTime = Instant.now();
 
             ZonedDateTime erasingTime;
 
             broadcastMessage.setFiringTime(
-                    futureTimestamp(
-                            firingTime,
+                    getInstant(
+                            broadcastMessageUpdate.getFiringTime(),
                             currentTime,
                             broadcastMessage.getId(),
+                            Error.BROADCAST_MESSAGE_FIRING_TIME_IS_MALFORMED,
                             Error.BROADCAST_MESSAGE_FIRING_TIME_IS_IN_PAST
                     )
             );
 
             if (broadcastMessageUpdate.getErasingTime() != null) {
-
-                erasingTime = getDateTimeAtLocalZone(
-                        broadcastMessageUpdate.getErasingTime(),
-                        Error.BROADCAST_MESSAGE_FIRING_TIME_IS_MALFORMED
-                );
-
                 broadcastMessage.setErasingTime(
-                        futureTimestamp(
-                                erasingTime,
-                                firingTime,
+                        getInstant(
+                                broadcastMessageUpdate.getErasingTime(),
+                                broadcastMessage.getFiringTime(),
                                 broadcastMessage.getId(),
+                                Error.BROADCAST_MESSAGE_FIRING_TIME_IS_MALFORMED,
                                 Error.BROADCAST_MESSAGE_ERASING_TIME_IS_BEFORE_THEN_FIRING_TIME
                         )
                 );
--- src/main/java/chat/tamtam/bot/domain/broadcast/message/action/SentStateAction.java
+++ src/main/java/chat/tamtam/bot/domain/broadcast/message/action/SentStateAction.java
@@ -1,6 +1,6 @@
 package chat.tamtam.bot.domain.broadcast.message.action;
 
-import java.time.ZonedDateTime;
+import java.time.Instant;
 
 import org.springframework.stereotype.Component;
 
@@ -34,18 +34,14 @@ public final class SentStateAction extends BroadcastMessageStateAction {
             return;
         }
 
-        ZonedDateTime currentTime = ZonedDateTime.now(SERVER_LOCAL_ZONE_ID);
-
-        ZonedDateTime erasingTime = getDateTimeAtLocalZone(
-                broadcastMessageUpdate.getErasingTime(),
-                Error.BROADCAST_MESSAGE_ERASING_TIME_IS_MALFORMED
-        );
+        Instant currentTime = Instant.now();
 
         broadcastMessage.setErasingTime(
-                futureTimestamp(
-                        erasingTime,
+                getInstant(
+                        broadcastMessageUpdate.getErasingTime(),
                         currentTime,
                         broadcastMessage.getId(),
+                        Error.BROADCAST_MESSAGE_ERASING_TIME_IS_MALFORMED,
                         Error.BROADCAST_MESSAGE_ERASING_TIME_IS_IN_THE_PAST
                 )
         );
