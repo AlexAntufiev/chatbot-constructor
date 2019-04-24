a8e510f47f0fd2dcfa310ebd838497f1ed8f2d12
gorbatov <gorbatov.eg@gmail.com>
2019-04-25 00:20:37 +0300

    Regbot url api.

--- src/main/java/chat/tamtam/bot/controller/ResourcesController.java
+++ src/main/java/chat/tamtam/bot/controller/ResourcesController.java
@@ -4,21 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import chat.tamtam.bot.service.UserService;
+import chat.tamtam.bot.service.ResourcesService;
 import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(path = Endpoints.RESOURCES, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(
+        path = Endpoint.RESOURCES,
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+)
 public class ResourcesController {
     private final ResourcesService resourcesService;
 
-    @GetMapping(Endpoint.REGISTRATION_BOT_URL, consumes = MediaType.ALL_VALUE)
+    @GetMapping(path = Endpoint.REGISTRATION_BOT_URL, consumes = MediaType.ALL_VALUE)
     public ResponseEntity<?> getRegistrationBotUrl() {
-        return new ResponseEntity<>(resourceService.getRegistrationBotUrl(), HttpStatus.OK); 
+        return new ResponseEntity<>(resourcesService.getRegistrationBotUrl(), HttpStatus.OK);
     }
 }
