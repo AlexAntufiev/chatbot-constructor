f92bb955375223ca1d71a5336294542a37f63f58
gorbatov <gorbatov.eg@gmail.com>
2019-05-01 16:57:11 +0300

    Revert "Revert "[CC-165] Implement hockey 2019 service (#166)""
    This reverts commit e065c3f35fdcc5562cfce6fb4850e1d63d2143e3.

--- src/test/java/chat/tamtam/bot/RunnableTestContext.java
+++ src/test/java/chat/tamtam/bot/RunnableTestContext.java
@@ -1,8 +1,11 @@
 package chat.tamtam.bot;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 
+@TestConfiguration
 @SpringBootTest
 public class RunnableTestContext extends TestContext {
 
 }
+
