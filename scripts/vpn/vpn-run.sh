dbdfc7866e03e729dc1e860b2cc4f816b40db282
gorbatov <gorbatov.eg@gmail.com>
2019-05-04 01:44:17 +0300

    PR fix.

--- scripts/vpn/vpn-run.sh
+++ scripts/vpn/vpn-run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-
+docker_container_name=ipsec-vpn-server
 docker pull hwdsl2/ipsec-vpn-server
 
 sudo modprobe af_key
 
+docker rm -f ${docker_container_name}
+
 docker run \
-    --name ipsec-vpn-server \
+    --name ${docker_container_name} \
     --env-file ./vpn.env \
     --restart=always \
     -p 500:500/udp \
