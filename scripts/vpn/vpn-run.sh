6324f341d215fd4a28f1d63872c6756034fd8f74
gorbatov <gorbatov.eg@gmail.com>
2019-05-04 00:57:14 +0300

    IPsec vpn simple script.

---
+++ scripts/vpn/vpn-run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker pull hwdsl2/ipsec-vpn-server
+
+sudo modprobe af_key
+
+docker run \
+    --name ipsec-vpn-server \
+    --env-file ./vpn.env \
+    --restart=always \
+    -p 500:500/udp \
+    -p 4500:4500/udp \
+    -v /lib/modules:/lib/modules:ro \
+    -d --privileged \
+    hwdsl2/ipsec-vpn-server
