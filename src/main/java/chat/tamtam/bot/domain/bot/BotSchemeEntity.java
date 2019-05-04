331459847c85556515a8ceaba74b15bca4afdb32
Aleksey Antufev <Aleksey.Antufev@billing.ru>
2019-05-04 22:29:23 +0300

    [CC-204] GenerationType.IDENTITY -> GenerationType.AUTO

--- src/main/java/chat/tamtam/bot/domain/bot/BotSchemeEntity.java
+++ src/main/java/chat/tamtam/bot/domain/bot/BotSchemeEntity.java
@@ -19,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BotSchemeEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
 
