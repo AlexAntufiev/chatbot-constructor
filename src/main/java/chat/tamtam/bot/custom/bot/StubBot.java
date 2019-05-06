2aee51a26dbdd1856c4d48af840c91b340353e2f
Egor Gorbatov <gorbatov.eg@gmail.com>
2019-05-06 23:55:18 +0300

    [CC-223] Fix custom bots refreshing scope (#224)

--- src/main/java/chat/tamtam/bot/custom/bot/StubBot.java
+++ src/main/java/chat/tamtam/bot/custom/bot/StubBot.java
@@ -4,8 +4,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class StubBot extends AbstractCustomBot {
+    public StubBot() {
+        super(null, null, null, null);
+    }
+
     @Override
     public BotType getType() {
         return BotType.Stub;
     }
+
+    @Override
+    protected void subscribe() { }
+
+    @Override
+    public void unsubscribe() { }
 }
