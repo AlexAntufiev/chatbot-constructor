f65a1f3e2999c5ce079e058e7eafb0b8bd869399
Alex Antufev <12766383+AlexAntufiev@users.noreply.github.com>
2019-05-07 19:22:09 +0300

    #218 Release version 1.0.0 (#219)
    * #218 Release version 1.0.0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 app_name="chatbot-constructor"
-ssh_deploy_file_test="/tmp/test_ssh"
+ssh_deploy_file_test="C:\Users\Aleksey.Antufev\Documents\chatbot-constructor\keys\TEST-1.pem"
 ssh_deploy_file_prod="/tmp/prod_ssh"
 user="centos"
 server_type=$1
 server=$2
+db_password=MDYwNTIwMTlxd2VydHkK
 
 if [ "$server_type" == "test" ]; then
     ssh_path=${ssh_deploy_file_test}
@@ -17,7 +18,11 @@ else
 fi
 
 cp ./build/libs/chatbot-constructor.jar ./docker/chatbot-constructor.jar
-rsync -avz --progress -e "ssh -i ${ssh_path}" ./docker/ ${user}@${server}:/home/${user}/${app_name}
+cd ./docker/
+printf "profile=${server_type}" > app.env
+printf "POSTGRES_USER=${server_type}\nPOSTGRES_PASSWORD=${db_password}" > db.env
+
+rsync -avz --progress -e "ssh -i ${ssh_path}" ./ ${user}@${server}:/home/${user}/${app_name}
 time ssh -i ${ssh_path} ${user}@${server} "cd ${app_name} && \
-                                           chmod +x docker.sh run.sh && \
+                                           chmod +x docker.sh && \
                                            nohup ./docker.sh ${server_type} ${server} > /dev/null 2>&1 &"
