4875e6de43f77bab736cb9f88f1d5d3ef1073d52
Egor Gorbatov <gorbatov.eg@gmail.com>
2019-05-13 20:14:19 +0300

    [CC-264] Disconnect tamtam bot before scheme deletion. (#265)

--- src/main/java/chat/tamtam/bot/service/BotSchemeService.java
+++ src/main/java/chat/tamtam/bot/service/BotSchemeService.java
@@ -3,12 +3,15 @@ package chat.tamtam.bot.service;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import chat.tamtam.bot.configuration.logging.Loggable;
+import chat.tamtam.bot.controller.Endpoint;
 import chat.tamtam.bot.domain.bot.BotScheme;
 import chat.tamtam.bot.domain.bot.TamBotEntity;
 import chat.tamtam.bot.domain.builder.component.SchemeComponent;
+import chat.tamtam.bot.domain.exception.ChatBotConstructorException;
 import chat.tamtam.bot.domain.exception.NotFoundEntityException;
 import chat.tamtam.bot.domain.response.SuccessResponse;
 import chat.tamtam.bot.domain.response.SuccessResponseWrapper;
@@ -16,9 +19,15 @@ import chat.tamtam.bot.repository.BotSchemeRepository;
 import chat.tamtam.bot.repository.ComponentRepository;
 import chat.tamtam.bot.repository.TamBotRepository;
 import chat.tamtam.bot.utils.TransactionalUtils;
+import chat.tamtam.botapi.TamTamBotAPI;
+import chat.tamtam.botapi.exceptions.APIException;
+import chat.tamtam.botapi.exceptions.ClientException;
+import chat.tamtam.botapi.model.SimpleQueryResult;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class BotSchemeService {
@@ -29,6 +38,9 @@ public class BotSchemeService {
     private final @NonNull UserService userService;
 
     private final @NonNull TransactionalUtils transactionalUtils;
+
+    @Value("${tamtam.host}")
+    private String host;
 
     @Loggable
     public BotScheme addBot(final BotScheme bot, Long userId) throws IllegalArgumentException {
@@ -92,12 +104,51 @@ public class BotSchemeService {
                         new TamBotEntity.Id(botScheme.getBotId(), botScheme.getUserId())
                 );
                 if (tamBot != null) {
+                    disconnect(tamBot, botScheme.getId());
                     tamBotRepository.deleteById(tamBot.getId());
                 }
             }
             botSchemeRepository.deleteByUserIdAndId(botScheme.getUserId(), botScheme.getId());
         });
         return new SuccessResponse();
+    }
+
+    private void disconnect(final TamBotEntity tamBot, final int schemeId) {
+        TamTamBotAPI tamTamBotAPI = TamTamBotAPI.create(tamBot.getToken());
+        try {
+            final String url = host + Endpoint.TAM_BOT + "/" + schemeId;
+
+            SimpleQueryResult result =
+                    tamTamBotAPI
+                            .unsubscribe(url)
+                            .execute();
+
+            if (result.isSuccess()) {
+                log.info(
+                        String.format(
+                                "Bot(id=%d, token=%s, botSchemeId=%d, url=%s) was unsubscribed",
+                                tamBot.getId().getBotId(), tamBot.getToken(), schemeId, url
+                        )
+                );
+            } else {
+                throw new ChatBotConstructorException(
+                        String.format(
+                                "Can't unsubscribe bot with id=%d because success=%s",
+                                schemeId, result.isSuccess()
+                        ),
+                        Error.TAM_SERVICE_ERROR
+                );
+            }
+        } catch (ClientException | APIException e) {
+            throw new ChatBotConstructorException(
+                    String.format(
+                            "Can't unsubscribe bot with id=%d because %s",
+                            schemeId, e.getLocalizedMessage()
+                    ),
+                    Error.TAM_SERVICE_ERROR,
+                    e
+            );
+        }
     }
 
     @Loggable
