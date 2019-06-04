43d2266ba9293622dae9616d1d71524ef1877da7
Aleksey Antufev <Aleksey.Antufev@billing.ru>
2019-06-05 02:31:23 +0300

    delete logger

--- src/main/java/chat/tamtam/bot/configuration/DiscoveryProperties.java
+++ src/main/java/chat/tamtam/bot/configuration/DiscoveryProperties.java
@@ -5,9 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RefreshScope
 @Configuration
 public class DiscoveryProperties {
