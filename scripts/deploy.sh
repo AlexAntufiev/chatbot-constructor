ead8fb5f4a2e7cda89ee9f5be2d6dd608d290d46
Aleksey Antufev <antufev@yamoney.ru>
2020-05-06 00:15:38 +0300

    build app everytime

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,4 +12,4 @@ printf "POSTGRES_USER=${db_user}\nPOSTGRES_PASSWORD=${db_password}" > db.env
 
 time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "mkdir -p ${app_name} && cd ${app_name} && rm -f ${app_name}*"
 rsync -avz -e "ssh -i ${ssh_path}" ./ ${server_user}@${PROD_IP}:/home/${server_user}/${app_name}
-time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "cd ${app_name} && docker-compose up -d"
+time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "cd ${app_name} && docker-compose build --force-rm app && docker-compose up -d"
