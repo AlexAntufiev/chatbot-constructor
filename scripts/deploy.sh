f7b639b283e0d3e7130612dcd275c0b328ed5539
Aleksey Antufev <antufev@yamoney.ru>
2020-05-17 20:35:34 +0300

    build docker image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,8 @@ ssh_path="/tmp/prod_ssh.pem"
 echo "${PROD_SSH}" | base64 --decode >${ssh_path}
 chmod 600 ${ssh_path}
 
-cp ./build/libs/chatbot-constructor* ./docker/
-cd ./docker/
-printf "POSTGRES_USER=${db_user}\nPOSTGRES_PASSWORD=${db_password}" > db.env
+printf "POSTGRES_USER=${db_user}\nPOSTGRES_PASSWORD=${db_password}" > ./docker/db.env
 
-time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "mkdir -p ${app_name} && cd ${app_name} && rm -f ${app_name}*"
-rsync -avz -e "ssh -i ${ssh_path}" ./ ${server_user}@${PROD_IP}:/home/${server_user}/${app_name}
+time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "mkdir -p ${app_name}"
+rsync -avz -e "ssh -i ${ssh_path}" ./docker/ ${server_user}@${PROD_IP}:/home/${server_user}/${app_name}
 time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "cd ${app_name} && docker-compose build --force-rm app && docker-compose up -d"
