3e93c76cc9ffd074e249874c692996fadf85e260
Aleksey Antufev <antufev@yamoney.ru>
2020-05-18 13:57:59 +0300

    use new app dir

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,6 @@ ssh_path="/tmp/prod_ssh.pem"
 echo "${PROD_SSH}" | base64 --decode >${ssh_path}
 chmod 600 ${ssh_path}
 
-printf "POSTGRES_USER=${db_user}\nPOSTGRES_PASSWORD=${db_password}" > ./docker/db.env
-
-time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "mkdir -p ${app_name}"
-rsync -avz -e "ssh -i ${ssh_path}" ./docker/ ${server_user}@${PROD_IP}:/home/${server_user}/${app_name}
+printf "POSTGRES_USER=${db_user}\nPOSTGRES_PASSWORD=${db_password}" > ./${app_name}/db.env
+rsync -avz -e "ssh -i ${ssh_path}" ./${app_name}/ ${server_user}@${PROD_IP}:/home/${server_user}/${app_name}
 time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "cd ${app_name} && docker-compose build --force-rm app && docker-compose up -d"
