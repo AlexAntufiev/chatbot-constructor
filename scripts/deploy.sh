4debeb5c55d3f9032bde2ee65b4c04713b9258cb
Aleksey Antufev <antufev@yamoney.ru>
2020-05-18 21:31:38 +0300

    delete rebuild image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,4 @@ chmod 600 ${ssh_path}
 
 printf "POSTGRES_USER=${db_user}\nPOSTGRES_PASSWORD=${db_password}" > ./${app_name}/db.env
 rsync -avz -e "ssh -i ${ssh_path}" ./${app_name}/ ${server_user}@${PROD_IP}:/home/${server_user}/${app_name}
-time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "cd ${app_name} && docker-compose build --force-rm app && docker-compose up -d"
+time ssh -i ${ssh_path} ${server_user}@${PROD_IP} "cd ${app_name} && docker-compose up -d"
